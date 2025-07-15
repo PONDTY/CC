aedfc21bbd61b99703eb00c692f0066782bf8ed9
PONDTY <100811351+PONDTY@users.noreply.github.com>
2025-07-15 20:40:35 +0700

    Update product.py

--- mobile-mining/product.py
+++ mobile-mining/product.py
@@ -8,7 +8,7 @@ def product():
     os.system("figlet -f digital server version")
     #print("\033[00m\n")
     print("\033[96mDeveloper by PICHET SAENGTAWAN\033[0m")
-    #print("\033[96m\033[0m")
+    print("\033[96mEDIT To VP\033[0m")
     #print("\033[36m\033[0m")
 
     #print(f"\n\033[91mกำลัง Run Mining\033[0m\n")
